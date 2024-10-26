c260c6e2d339e14ec7f069866ebe13a114493410
mrmartuz <mattia92martinelli@gmail.com>
2024-10-26 17:40:07 +0200

    fix: add current year var

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
 import React from "react";
 
-export function Footer(){
-    return (
-        <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 FitTrack. All rights reserved.</p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Terms of Service
-          </Link>
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Privacy
-          </Link>
-        </nav>
-      </footer>
-    );
+export function Footer() {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  return (
+    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <p className="text-xs text-gray-500 dark:text-gray-400">© {currentYear} Fitbeat. All rights reserved.</p>
+      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link className="text-xs hover:underline underline-offset-4" href="#">
+          Terms of Service
+        </Link>
+        <Link className="text-xs hover:underline underline-offset-4" href="#">
+          Privacy
+        </Link>
+      </nav>
+    </footer>
+  );
 }
