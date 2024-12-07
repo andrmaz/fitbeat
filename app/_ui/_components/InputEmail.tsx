5fd38ced052b310fcb6e705610af75f14456e012
andrmaz <60042277+andrmaz@users.noreply.github.com>
2024-12-07 15:19:44 +0100

    feat: email

--- app/_ui/_components/InputEmail.tsx
+++ app/_ui/_components/InputEmail.tsx
@@ -4,7 +4,14 @@ export function InputEmail() {
   return (
     <>
       <label htmlFor='email'>Email</label>
-      <Input type='email' placeholder='example@email' name='email' id='email' />
+      <Input
+        type='email'
+        placeholder='example@email'
+        name='email'
+        id='email'
+        required
+        className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'
+      />
     </>
   )
 }
